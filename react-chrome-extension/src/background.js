c306ca8ff0ceb097e1b46a008079b114acb63086
michael.rode <michael.rode@rigup.com>
2020-01-18 02:36:11 -0600

    WIP: Trying to render app w/ button overlay

--- react-chrome-extension/src/background.js
+++ react-chrome-extension/src/background.js
@@ -5,9 +5,11 @@ const clearStorage = () => {
     console.log('Cleared Storage');
   });
 };
+
 chrome.tabs.onUpdated.addListener(function(tabId, change, tab) {
   console.log('onUpdated: ' + tab.url);
   if (tab.url) {
+    chrome.tabs.sendMessage(tab.id, { type: 'getPosts', url: tab.url });
     chrome.storage.local.set({ key: tab.url }, function() {
       console.log('Setting Saved', tab.url);
     });
@@ -25,6 +27,10 @@ chrome.tabs.onSelectionChanged.addListener(function(tabId, info) {
     const currentTab = tab[0];
     console.log('tab query', currentTab);
     if (currentTab.url) {
+      chrome.tabs.sendMessage(currentTab.id, {
+        type: 'getPosts',
+        url: tab.url
+      });
       chrome.storage.local.set({ key: currentTab.url }, function() {
         console.log('Setting Saved', currentTab.url);
       });
--- react-chrome-extension/src/contentScript.js
+++ react-chrome-extension/src/contentScript.js
@@ -1,33 +1,41 @@
+// Show button to display comments in bottom right corner if they are present.
 // USE THIS ONE!
 /*global chrome*/
-let div = document.createElement('div');
-div.id = 'contentScriptDivID';
-div.style.backgroundColor = '#31B0D5';
-div.style.color = 'white';
-div.style.color = 'white';
-div.style.padding = '10px 20px';
-div.style.borderRadius = '4px';
-div.style.borderColor = '#46b8da';
-div.innerHTML = 'React-Chrome-Extension';
-
-let btn = document.createElement('button');
-btn.id = 'contentScriptButtonID';
-btn.innerHTML = 'Click Me!';
-chrome.storage.local.get(null, function(result) {
-  document.dispatchEvent(new CustomEvent('csEvent', { detail: result }));
-  console.log('Storage --', result);
+chrome.runtime.onMessage.addListener(request => {
+  if (request.type === 'getPosts') {
+    // alert(JSON.stringify(request.url));
+    // DO SOMETHING
+  }
 });
-div.appendChild(btn);
-document.body.prepend(div);
 
-btn.addEventListener('click', function() {
-  console.log('Starting button event handler');
-  chrome.runtime.sendMessage(
-    { type: 'GET', params: { token: 'token' } },
-    {},
-    function(response) {
-      console.log('Inside sendMessage');
-      console.log('Content script response: ', response);
-    }
-  );
-});
+// let div = document.createElement('div');
+// div.id = 'contentScriptDivID';
+// div.style.backgroundColor = '#31B0D5';
+// div.style.color = 'white';
+// div.style.color = 'white';
+// div.style.padding = '10px 20px';
+// div.style.borderRadius = '4px';
+// div.style.borderColor = '#46b8da';
+// div.innerHTML = 'React-Chrome-Extension';
+
+// let btn = document.createElement('button');
+// btn.id = 'contentScriptButtonID';
+// btn.innerHTML = 'Click Me!';
+// chrome.storage.local.get(null, function(result) {
+//   document.dispatchEvent(new CustomEvent('csEvent', { detail: result }));
+//   console.log('Storage --', result);
+// });
+// div.appendChild(btn);
+// document.body.prepend(div);
+
+// btn.addEventListener('click', function() {
+//   console.log('Starting button event handler');
+//   chrome.runtime.sendMessage(
+//     { type: 'GET', params: { token: 'token' } },
+//     {},
+//     function(response) {
+//       console.log('Inside sendMessage');
+//       console.log('Content script response: ', response);
+//     }
+//   );
+// });
